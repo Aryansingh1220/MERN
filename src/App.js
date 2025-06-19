a6e50b80f399472febb029bdc0d521b6472f9951
Aryan singh <aryansinghup6562@gmail.com>
2025-06-19 10:07:29 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Login from './Login';
-import Home from './Home';
-
+import { Navigate, Route,Routes } from "react-router-dom";
+import Login from "./Login";
+import Home from "./Home";
+import AppLayout from "./layout/Applayout";
+import Dashboard from "./pages/Dashboard";
+// import { Nav } from "react-bootstrap";
+import { useState } from "react";
 function App() {
+  const [userdetails,setUserDetails]=useState(null);
+
+  const updateUserDetails=(UpdatedData)=>{
+    setUserDetails(UpdatedData);
+  }
   return (
-    <div className="App">
+    
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={userdetails ?
+          <Navigate to='/dashboard'/>:
+          <AppLayout>
+            <Home />
+          </AppLayout>
+        }/>
+        <Route path="/login" element={userdetails?
+          <Navigate to='/dashboard'/>:
+          <AppLayout>
+            <Login updateUserDetails={updateUserDetails} />
+          </AppLayout>
+        }/>
+        <Route path="/dashboard" element={userdetails ?
+          <Dashboard/>:
+          <Navigate to='/login' />
+        }/> 
+
       </Routes>
-    </div>
   );
 }
 
